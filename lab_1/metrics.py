22e576cd3f70d42c6feebe4eb4ad6e9ee768bdbb
VSydorskyy <vladimirsydorskyy@gmail.com>
2019-09-13 17:07:36 +0300

    metrics added

--- lab_1/metrics.py
+++ lab_1/metrics.py
@@ -5,8 +5,20 @@ def get_coef_matrix_from_parsing_results(parsing_results):
     return np.array([parsing_results['out_weights'].get(i, 0.) for i in range(signal_time_shift+1)] + \
                     [parsing_results['manage_weights'].get(i, 0.) for i in range(signal_time_shift+1)])
 
-def mse(y_true, y_pred):
+def rmse(y_true, y_pred):
     return np.linalg.norm(y_true - y_pred)
 
+def rsse(y_true, y_pred):
+    return ((y_true - y_pred)**2).sum()
+
+def dispersion(distribution):
+    return ((distribution - distribution.mean())**2).mean()
+
+def determination_coef(y_true, y_pred):
+    return dispersion(y_pred)/dispersion(y_true)
+
+def akkake_criteria(y_true, y_pred, model_params):
+    return y_true.shape[0]*np.log(rsse(y_true,y_pred)) + 2*model_params
+
 def count_model_coef_rmse(model, parsing_results):
-    return mse(get_coef_matrix_from_parsing_results(parsing_results), model.coef_matrix.squeeze())
+    return rmse(get_coef_matrix_from_parsing_results(parsing_results), model.coef_matrix.squeeze())
