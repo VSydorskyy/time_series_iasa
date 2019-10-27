1dc41abc34b0da9bf3cb7a12a8ad86c85069d662
--replace-all <vs@respeecher.com>
2019-10-27 09:17:12 +0200

    Lab2 fixes

--- lab_2/.ipynb_checkpoints/MA-checkpoint.py
+++ lab_2/.ipynb_checkpoints/MA-checkpoint.py
@@ -6,7 +6,7 @@ def moving_average(a, n=3, weights=None):
     if weights == 'exp':
         weights = np.exp(-np.arange(start=0,stop=n))
     elif weights == 'exp_smoothed':
-        weights = np.arange(start=0,stop=n)**(1 - 2/(n+1))
+        weights = (1 - 2/(n+1))**np.arange(start=0,stop=n)
     else:
         weights = np.ones(n)
     
@@ -15,4 +15,4 @@ def moving_average(a, n=3, weights=None):
     for i in range(len(a)-n+1):
         mov_avarage.append(a[i:i+n]@weights / sum(weights))
         
-    return np.array(mov_avarage)
+    return np.array(mov_avarage), weights
